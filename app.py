82ca5ad73db7fad35f9b1a17e669a5e7cc6b50d0
Akash Kumar Singh <akashqp@gmail.com>
2024-09-30 05:57:56 +0530

    Major changes

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template
+import logging
 
 app = Flask(__name__)
 
+logging.basicConfig(filename='app.log', level=logging.INFO)
+
 @app.route('/')
 def home():
+    logging.info('Home page accessed')
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+# @app.route('/about')
+# def about():
+#     return render_template('about.html')
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0')
